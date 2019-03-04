6fad64801c5242995597562e388e33b51909acee
Lucia <15171258@itculiacan.edu.mx>
2019-03-04 11:35:27 -0700

    Primera app

---
+++ PrimeraApp/PrimeraApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrimeraApp
+//
+//  Created by mac11 on 04/03/19.
+//  Copyright © 2019 mac11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
